23c9663a88d96ce2bb228fcc8c6255c086ad7bf3
Antonio Taminto <taminto.a@gmail.com>
2024-01-06 17:35:19 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 //L'array ottenuto dovrà essere stampato a video.
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,2,1,2,1,2};
+        int[] array = {1,2,1,2,1,2,3,4,85,2,2,5,3,4,5};
         noDuplicati(array);
     }
     public static void noDuplicati(int[] array){
